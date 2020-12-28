b5cac861e367f90857e93644004192f3a733d677
Fernando Barajas <fernyb@fernyb.net>
2020-12-27 17:51:03 -0800

    custom matcher

--- test/todomvc.test.js
+++ test/todomvc.test.js
@@ -2,6 +2,42 @@ const Pup = require("../lib/index.js").Pup;
 const Todo = require("../pages/todo.js");
 
 
+expect.extend({
+  toBeCookie(expected) {
+    if (expected === null) {
+      return {
+        pass: false,
+        message: () => 'Cookie expected to be not null.'
+      };
+    }
+    let keys = Object.keys(expected);
+
+    if (this.isNot) {
+      expect(keys).not.toContain("name");
+      expect(keys).not.toContain("value");
+      expect(keys).not.toContain("domain");
+      expect(keys).not.toContain("path");
+      expect(keys).not.toContain("expires");
+      expect(keys).not.toContain("size");
+      expect(keys).not.toContain("httpOnly");
+      expect(keys).not.toContain("secure");
+      expect(keys).not.toContain("session");
+    } else {
+      expect(keys).toContain("name");
+      expect(keys).toContain("value");
+      expect(keys).toContain("domain");
+      expect(keys).toContain("path");
+      expect(keys).toContain("expires");
+      expect(keys).toContain("size");
+      expect(keys).toContain("httpOnly");
+      expect(keys).toContain("secure");
+      expect(keys).toContain("session");
+    }
+
+    return { pass: !this.isNot };
+  }
+});
+
 describe("TodoMVC", () => {
 var p = null;
 
@@ -79,6 +115,9 @@ test("add/delete todo items", async (done) => {
     expect(keys).toContain("httpOnly");
     expect(keys).toContain("secure");
     expect(keys).toContain("session");
+
+    // use a custom matcher
+    expect(cookie).toBeCookie();
   });
 
   cookies = await todoPage.cookies();
