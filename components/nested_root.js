368f2873d103502f4b763dc9fd816549f7513001
Fernando Barajas <fbarajas@servicetitan.com>
2021-01-14 11:19:20 -0800

    add test and support for find within

--- components/nested_root.js
+++ components/nested_root.js
@@ -1,5 +1,54 @@
 const P = require("../lib/index.js");
 
+class SectionNode extends P.PupElement {
+  constructor(context, element) {
+    super(context, element);
+  }
+
+  get rootQuery() {
+    return "section[name=top]";
+  }
+
+  async headerNode() {
+    let nodes = await this.findAndWaitForElements("div.header");
+    return new HeaderNode(this, nodes[0]);
+  }
+}
+
+class HeaderNode extends P.PupElement {
+  constructor(context, element) {
+    super(context, element);
+  }
+
+  get rootQuery() {
+    return "div.header";
+  }
+
+  async headerTitleNode() {
+    let nodes = await this.findAndWaitForElements("h3");
+    return new P.PupElement(this, nodes[0]);
+  }
+}
+
+class NestChild extends P.PupElement {
+  constructor(context, element) {
+    super(context, element);
+  }
+
+  get rootQuery() {
+    return ".nest_child";
+  }
+
+  async hasImage() {
+    let nodes = await this.allImageElements();
+    return nodes.length > 0;
+  }
+
+  async allImageElements() {
+    return await this.findAndWaitForElements("img");
+  }
+}
+
 class NestedRoot extends P.PupElement {
   constructor(context, element) {
     super(context, element);
@@ -12,6 +61,21 @@ class NestedRoot extends P.PupElement {
   async firstParagraphContainer() {
     let nodes = await this.findAndWaitForElements("p");
     return new P.PupElement(this, nodes[0]);
+  }
+
+  async allParagraphContainers() {
+    let nodes = await this.findAndWaitForElements("p");
+    return nodes.map(x => new P.PupElement(this, x));
+  }
+
+  async nestedChildrenContainers() {
+    let nodes = await this.findAndWaitForElements(".nest_child");
+    return nodes.map(x => new NestChild(this, x));
+  }
+
+  async topSectionNode() {
+    let nodes = await this.findAndWaitForElements("section[name=top]");
+    return new SectionNode(this, nodes[0]);
   }
 }
 
--- test/nested_finders.test.js
+++ test/nested_finders.test.js
@@ -18,8 +18,48 @@ describe("Nested Finders", () => {
     let examplePage = new ExamplePage(page);
     let nestedRoot = await examplePage.nestedRoot();
 
-    let textContainer = await nestedRoot.firstParagraphContainer();
-    console.log(textContainer);
+    // find paragraph tags within the nestedRoot container
+    let firstParagraphContainer = await nestedRoot.firstParagraphContainer();
+    expect(await firstParagraphContainer.innerText()).toEqual("read all about it.");
+
+    let allParagraphElements = await nestedRoot.allParagraphContainers();
+    expect(allParagraphElements.length).toBe(4);
+
+    let sectionNode = await nestedRoot.topSectionNode();
+    let headerNode = await sectionNode.headerNode();
+    let titleNode = await headerNode.headerTitleNode();
+    expect(await titleNode.innerText()).toEqual("NEWS!");
+
+    done();
+  });
+
+  test("find images", async (done) => {
+    let page = await p.newPage("http://localhost:3000/public/examples");
+    let examplePage = new ExamplePage(page);
+    let nestedRoot = await examplePage.nestedRoot();
+
+    let nestedChildren = await nestedRoot.nestedChildrenContainers();
+    expect(nestedChildren.length).toBe(3);
+
+    // verify we have an image on the first child
+    expect(
+      (await nestedChildren[0].allImageElements()).length
+    ).toEqual(1);
+
+    expect(await nestedChildren[0].hasImage()).toBeTruthy();
+
+    // verify we do not have images on the second child
+    expect(await nestedChildren[1].hasImage()).toBeFalsy();
+    expect(await nestedChildren[2].hasImage()).toBeFalsy();
+
+    expect(
+      (await nestedChildren[1].allImageElements()).length
+    ).toEqual(0);
+
+    expect(
+      (await nestedChildren[2].allImageElements()).length
+    ).toEqual(0);
+
     done();
   });
 });
