9596f817d699af48f3dc79a99fe62d7951352db6
Fernando Barajas <fernyb@fernyb.net>
2021-01-15 19:31:53 -0800

    adding support for mobile emulation

---
+++ test/mobile_emulation.test.js
@@ -0,0 +1,50 @@
+const Pup = require("../lib/index.js").Pup;
+const ExamplePage = require("../pages/example.js");
+
+describe("PupApp Mobile", () => {
+  let p = null;
+
+  beforeAll(() => {
+    p = new Pup({
+      useMobileEmulation: true
+    });
+  });
+
+  afterAll(async () => {
+    await p.close();
+  });
+
+  test("clicking link should not navigate", async (done) => {
+    let page = await p.newPage("http://localhost:3000/public/examples");
+    await page.waitForSelector("#element-toggle");
+
+    let el = await page.findBySelector("a#no-click");
+    await el.click();
+
+    // verify we do not navigate
+    let h1 = await page.findElementWithTag("h1");
+    expect(await h1.innerText()).not.toEqual("Posts");
+
+    // toggle the element so the link is clickable
+    let btn = await page.findBySelector("button#element-toggle");
+    await btn.click();
+
+    await el.click();
+    await page.waitForText("Posts");
+
+    // we should be at the root page /
+    h1 = await page.findElementWithTag("h1");
+    expect(await h1.innerText()).toEqual("Active Posts");
+
+    await page.wait(3000);
+
+    done();
+  });
+
+  test("view source code", async (done) => {
+    let page = await p.newPage("https://github.com/puppeteer/puppeteer/blob/v5.5.0/src/common/DeviceDescriptors.ts");
+    await page.wait(10000);
+
+    done();
+  });
+});
