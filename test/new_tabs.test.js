1b3e2da9fcd2c16741ac7c0b21ccd4023acfda0f
Fernando Barajas <fernyb@fernyb.net>
2021-02-10 21:50:59 -0800

    more tests for tabs

--- test/new_tabs.test.js
+++ test/new_tabs.test.js
@@ -56,6 +56,59 @@ describe("New Tab", () => {
     expect(await p.pages()).toHaveLength(4);
     await examplePage.wait(2000);
 
+    await p.activateTabAtIndex(0);
+    await examplePage.wait(500);
+
+    await p.activateTabAtIndex(1);
+    await examplePage.wait(500);
+
+    await examplePage.activateTab();
+    await examplePage.wait(500);
+
+    await p.activateTabAtIndex(2);
+    await examplePage.wait(200);
+
+    await examplePage.activateTab();
+    await examplePage.wait(200);
+
+    await p.activateTabAtIndex(3);
+    await examplePage.wait(200);
+
+    await examplePage.activateTab();
+    await examplePage.wait(200);
+
+    await p.activateTabAtIndex(3);
+    await examplePage.wait(200);
+
+    await p.activateTabAtIndex(3);
+    await examplePage.wait(200);
+
+    await p.closePageAtIndex(3);
+    await examplePage.wait(500);
+
+    expect(await p.pages()).toHaveLength(3);
+
+    await examplePage.activateTab();
+    await examplePage.close();
+    await examplePage.wait(100);
+
+    expect(await p.pages()).toHaveLength(2);
+
+    let url = await (await p.pages())[0].url();
+    expect(url).toBe("about:blank");
+
+    url = await (await p.pages())[1].url();
+    expect(url).toBe("http://localhost:3000/");
+
+    const urls = (await p.pages()).map(async (el) => { 
+      return (await el.url());
+    });
+
+    const allUrls = await Promise.all(urls);
+
+    expect(allUrls[0]).toBe("about:blank");
+    expect(allUrls[1]).toBe("http://localhost:3000/");
+
     done();
   });
 });
