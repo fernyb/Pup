541da49d4811b8223d101c2f2251847152c46db5
Fernando Barajas <fernyb@fernyb.net>
2021-02-15 17:57:00 -0800

    update host

--- globals.js
+++ globals.js
@@ -4,4 +4,4 @@ global.ROOT_DIR = path.join(path.resolve(__dirname));
 global.SCREENSHOTS_DIR = path.join(path.resolve(__dirname), "screenshots");
 global.USER_DATA_DIR = path.join(path.resolve(__dirname), "user-data");
 global.DOWNLOAD_DIR = path.join(path.resolve(__dirname), "downloads");
-global.TEST_APP_HOST = "192.168.1.182";
+global.TEST_APP_HOST = "165.232.128.65";
